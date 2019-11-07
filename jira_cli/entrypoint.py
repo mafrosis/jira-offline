4a9333296c92f7582d6643676ad80034d99e1292
Matt Black <github@mafro.net>
2019-11-07 15:29:20 +1100

    Include stats command for issuetype and status

--- jira_cli/entrypoint.py
+++ jira_cli/entrypoint.py
@@ -2,8 +2,10 @@ import datetime
 import logging
 
 import click
+from tabulate import tabulate
 
 from jira_cli.main import Jira
+
 
 logger = logging.getLogger('jira')
 sh = logging.StreamHandler()
@@ -38,3 +40,25 @@ def cli_pull():
     dtstart = datetime.datetime.now()
     Jira.pull_issues()
     print('Query time: {}'.format(datetime.datetime.now() - dtstart))
+
+@cli.group(name='stats')
+def cli_group_stats():
+    'Generate stats on JIRA data'
+
+@cli_group_stats.command(name='issuetype')
+def cli_stats_issuetype():
+    '''Stats on issue type'''
+    df = Jira.load_issues()
+    aggregated_issuetype = df.groupby([df['issuetype']]).size().to_frame(name='count')
+    _print_table(aggregated_issuetype)
+
+@cli_group_stats.command(name='status')
+def cli_stats_status():
+    '''Stats on ticket status'''
+    df = Jira.load_issues()
+    aggregated_status = df.groupby([df['status']]).size().to_frame(name='count')
+    _print_table(aggregated_status)
+
+def _print_table(df):
+    '''Helper to pretty print dataframes'''
+    print(tabulate(df, headers='keys', tablefmt='psql'))
