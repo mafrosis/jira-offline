69c3ef06285a448e91931d3ee27e8ce6c5407c4b
Matt Black <github@mafro.net>
2019-11-12 07:37:06 +1100

    Implement --fix flag for fixVersions PI field

--- jira_cli/entrypoint.py
+++ jira_cli/entrypoint.py
@@ -90,12 +90,33 @@ def cli_group_lint_fixversions(ctx):
     df = jira.load_issues()
 
     if ctx.obj.fix:
-        logger.error('Not currently implemented')
-        return 1
+        # count of all issues missing the fixVersions field
+        initial_missing_count = len(df[df.fixVersions.apply(lambda x: len(x) == 0)])
 
-    print('There are {} issues missing the fixVersions field'.format(
-        len(df[df['fixVersions'].apply(lambda x: len(x) == 0)])
-    ))
+        # set PI5/PI6 on all relevant epics
+        for PI in ('PI5', 'PI6'):
+            # set PI into fixVersions field on Epics if their name matches
+            df[df.epic_name.str.contains(PI)].fixVersions.apply(lambda x: x.add(PI))  # pylint: disable=cell-var-from-loop
+
+            # iterate epics
+            for epic_ref in df[df.issuetype == 'Epic'].index:
+                # if PI value is set in fixVersions on the epic
+                if PI in jira[epic_ref].fixVersions:
+                    # filter all issues under this epic, and add PI to fixVersions
+                    df[df.epic_ref == jira[epic_ref].key].fixVersions.apply(
+                        lambda x: x.add(PI)  # pylint: disable=cell-var-from-loop
+                    )
+
+        # Write changes to local cache
+        jira.write_issues()
+
+        print('Updated fixVersions on {} issues'.format(
+            initial_missing_count - len(df[df.fixVersions.apply(lambda x: len(x) == 0)])
+        ))
+    else:
+        print('There are {} issues missing the fixVersions field'.format(
+            len(df[df['fixVersions'].apply(lambda x: len(x) == 0)])
+        ))
 
 
 def _print_table(df):
