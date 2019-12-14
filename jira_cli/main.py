mafrosis/jiracli

Add serialize/deserialize to Issue model

jira_cli/main.py
@@ -1,4 +1,9 @@
+import copy
+import dataclasses
 from dataclasses import dataclass, field
+import datetime
+import decimal
+import enum
 import json
 import os
 import urllib3
@@ -35,6 +40,63 @@ class Issue:
     estimate: int = field(default=None)
     epic_ref: str = field(default=None)
     epic_name: str = field(default=None)
+
+    @classmethod
+    def deserialize(cls, attrs: dict) -> object:
+        """
+        Deserialize JIRA API dict to dataclass
+        Support decimal, date/datetime & enum
+        """
+        data = copy.deepcopy(attrs)
+
+        for f in dataclasses.fields(cls):
+            v = attrs.get(f.name)
+            if v is None:
+                continue
+
+            if dataclasses.is_dataclass(f.type):
+                data[f.name] = f.type.deserialize(v)
+            elif f.type is decimal.Decimal:
+                data[f.name] = decimal.Decimal(v)
+            elif issubclass(f.type, enum.Enum):
+                # convert string to Enum instance
+                data[f.name] = f.type[v]
+            elif f.type is datetime.date:
+                data[f.name] = datetime.datetime.strptime(v, '%Y-%m-%d').date()
+            elif f.type is datetime.datetime:
+                data[f.name] = datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+
+        return cls(**data)
+
+    def serialize(self) -> dict:
+        """
+        Serialize dataclass to JIRA API dict
+        Support decimal, date/datetime & enum
+        Include only fields with repr=True (dataclass.field default)
+        """
+        data = {}
+
+        for f in dataclasses.fields(self):
+            if f.repr is False:
+                continue
+
+            v = self.__dict__.get(f.name)
+
+            if v is None:
+                data[f.name] = None
+            elif dataclasses.is_dataclass(f.type):
+                data[f.name] = v.serialize()
+            elif isinstance(v, decimal.Decimal):
+                data[f.name] = float(v)
+            elif issubclass(f.type, enum.Enum):
+                # convert Enum to raw string
+                data[f.name] = v.name
+            elif isinstance(v, (datetime.date, datetime.datetime)):
+                data[f.name] = v.isoformat()
+            else:
+                data[f.name] = v
+
+        return data
 
 
 class Jira:
@@ -79,7 +141,7 @@ class Jira:
 
         # dump issues to JSON cache
         json.dump(
-            {k:v.__dict__ for k,v in cls._issues.items()},
+            {k:v.serialize() for k,v in cls._issues.items()},
             open('issue_cache.json', 'w')
         )
         return cls._issues
@@ -93,7 +155,7 @@ class Jira:
         if issue.fields.fixVersions:
             fixVersions = [f.name for f in issue.fields.fixVersions]
 
-        return Issue(**{
+        return Issue.deserialize({
             'assignee': issue.fields.assignee.name if issue.fields.assignee else None,
             'created': issue.fields.created,
             'creator': issue.fields.creator.name,
@@ -126,7 +188,7 @@ class Jira:
         else:
             # load from cache file
             cls._issues = {
-                k:Issue(**v)
+                k:Issue.deserialize(v)
                 for k,v in json.load(open('issue_cache.json')).items()
             }
 
@@ -138,7 +200,7 @@ class Jira:
         Convert class variable to pandas DataFrame
         """
         df = pd.DataFrame.from_dict(
-            {key: issue.__dict__ for key, issue in cls._issues.items()}, orient='index'
+            {key: issue.serialize() for key, issue in cls._issues.items()}, orient='index'
         )
         df = df[ (df.issuetype != 'Delivery Risk') & (df.issuetype != 'Ops/Introduced Risk') ]
         return df

mafrosis/jira-offline

jira_cli/main.py
@@ -1,4 +1,9 @@
+import copy
+import dataclasses
 from dataclasses import dataclass, field
+import datetime
+import decimal
+import enum
 import json
 import os
 import urllib3
@@ -36,6 +41,63 @@ class Issue:
     estimate: int = field(default=None)
     epic_ref: str = field(default=None)
     epic_name: str = field(default=None)
+
+    @classmethod
+    def deserialize(cls, attrs: dict) -> object:
+        """
+        Deserialize JIRA API dict to dataclass
+        Support decimal, date/datetime & enum
+        """
+        data = copy.deepcopy(attrs)
+
+        for f in dataclasses.fields(cls):
+            v = attrs.get(f.name)
+            if v is None:
+                continue
+
+            if dataclasses.is_dataclass(f.type):
+                data[f.name] = f.type.deserialize(v)
+            elif f.type is decimal.Decimal:
+                data[f.name] = decimal.Decimal(v)
+            elif issubclass(f.type, enum.Enum):
+                # convert string to Enum instance
+                data[f.name] = f.type(v)
+            elif f.type is datetime.date:
+                data[f.name] = datetime.datetime.strptime(v, '%Y-%m-%d').date()
+            elif f.type is datetime.datetime:
+                data[f.name] = datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+
+        return cls(**data)
+
+    def serialize(self) -> dict:
+        """
+        Serialize dataclass to JIRA API dict
+        Support decimal, date/datetime & enum
+        Include only fields with repr=True (dataclass.field default)
+        """
+        data = {}
+
+        for f in dataclasses.fields(self):
+            if f.repr is False:
+                continue
+
+            v = self.__dict__.get(f.name)
+
+            if v is None:
+                data[f.name] = None
+            elif dataclasses.is_dataclass(f.type):
+                data[f.name] = v.serialize()
+            elif isinstance(v, decimal.Decimal):
+                data[f.name] = str(v)
+            elif issubclass(f.type, enum.Enum):
+                # convert Enum to raw string
+                data[f.name] = v.value
+            elif isinstance(v, (datetime.date, datetime.datetime)):
+                data[f.name] = v.isoformat()
+            else:
+                data[f.name] = v
+
+        return data
 
 
 class Jira:
@@ -80,7 +142,7 @@ class Jira:
 
         # dump issues to JSON cache
         json.dump(
-            {k:v.__dict__ for k,v in cls._issues.items()},
+            {k:v.serialize() for k,v in cls._issues.items()},
             open('issue_cache.json', 'w')
         )
         return cls._issues
@@ -94,7 +156,7 @@ class Jira:
         if issue.fields.fixVersions:
             fixVersions = [f.name for f in issue.fields.fixVersions]
 
-        return Issue(**{
+        return Issue.deserialize({
             'assignee': issue.fields.assignee.name if issue.fields.assignee else None,
             'created': issue.fields.created,
             'creator': issue.fields.creator.name,
@@ -127,7 +189,7 @@ class Jira:
         else:
             # load from cache file
             cls._issues = {
-                k:Issue(**v)
+                k:Issue.deserialize(v)
                 for k,v in json.load(open('issue_cache.json')).items()
             }
 