ff04a77383ab0fb3f6845df252dd5f5814d11c44
Matt Black <github@mafro.net>
2020-01-29 06:33:52 +1100

    Render Issue.diff_to_original in a separate method

--- jira_cli/models.py
+++ jira_cli/models.py
@@ -101,11 +101,34 @@ class Issue(DataclassSerializer):
             return True
         return False
 
+    def diff(self, data: dict=None) -> list:
+        '''
+        If this Issue object has the original property set, render the diff between self and
+        the original property. This is written to storage to track changes made offline.
+
+        Params:
+            data (optional):  Serialized dict of self
+        Returns:
+            List from dictdiffer.diff for Issue.diff_to_original property
+        '''
+        if not self.original:
+            return
+
+        if not data:
+            data = self.serialize()
+
+        return list(dictdiffer.diff(data, self.original, ignore=set(['diff_to_original'])))
+
     @classmethod
     def deserialize(cls, attrs: dict) -> object:
         '''
         Deserialize a dict into an Issue object. Inflate the original Jira issue from the
-        diff_to_original property
+        diff_to_original property.
+
+        Params:
+            attrs:  Dict to deserialize into an Issue
+        Returns:
+            List from dictdiffer.diff for Issue.diff_to_original property
         '''
         # deserialize supplied dict into an Issue object
         issue = super().deserialize(attrs)
@@ -115,7 +138,7 @@ class Issue(DataclassSerializer):
             issue.diff_to_original = []
 
         # apply the diff_to_original patch to the serialized version of the issue, which recreates
-        # the issue dict as last seen on the JIRA server
+        # the issue dict as last seen on the Jira server
         # pylint: disable=no-member
         issue.original = dictdiffer.patch(issue.diff_to_original, issue.serialize())
 
@@ -125,17 +148,15 @@ class Issue(DataclassSerializer):
         '''
         Serialize this Issue object to a dict. Generate the diff_to_original field which enables
         local changes to be written to the offline cache file.
+
+        Returns:
+            This Issue object as a serialized dict
         '''
         # serialize self (Issue object) into a dict
         data = super().serialize()
-
-        if self.original:
-            # if this Issue object has the original property set, render the diff between self and
-            # the original property. This is written to storage to track changes made offline.
-            data['diff_to_original'] = list(
-                dictdiffer.diff(data, self.original, ignore=set(['diff_to_original']))
-            )
-
+        diff_to_original = self.diff(data)
+        if diff_to_original:
+            data['diff_to_original'] = diff_to_original
         return data
 
     def __str__(self, conflicts: dict=None):
