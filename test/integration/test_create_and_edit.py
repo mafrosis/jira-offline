13c7b8711563739f2fee8a1d667257ef5389c560
Matt Black <github@mafro.net>
2020-04-25 14:51:45 +1000

    [fix] Bug fix the call to update_issue on push

--- test/integration/test_create_and_edit.py
+++ test/integration/test_create_and_edit.py
@@ -32,3 +32,38 @@ def test_create_new_issue_and_edit_before_push(jira_project, run_in_docker):
 
     # ensure edit we successful
     assert 'egg' in data['labels'] and 'bacon' in data['labels']
+
+
+@pytest.mark.integration
+def test_edit_existing_issue_and_push(jira_project, run_in_docker):
+    '''
+    Ensure existing Story can be edited, and then pushed successfully
+    '''
+    def setup():
+        # create a new issue
+        output = run_in_docker(jira_project, f'new --json {jira_project} Story EGGpic')
+
+        # push to Jira
+        output = run_in_docker(jira_project, '--verbose push')
+        assert 'INFO: Pushed 1 of 1 issues' in output
+
+    # setup Jira for test
+    setup()
+
+    # load and parse all current issues (there will be only 1!)
+    output = run_in_docker(jira_project, f'ls --json')
+    data = json.loads(output)
+
+    # edit the issue
+    run_in_docker(jira_project, f'edit {data["key"]} --labels=egg,bacon')
+
+    # push to Jira
+    output = run_in_docker(jira_project, '--verbose push')
+    assert 'INFO: Pushed 1 of 1 issues' in output
+
+    # load and parse all current issues (there will be only 1!)
+    output = run_in_docker(jira_project, f'ls --json')
+    data = json.loads(output)
+
+    # ensure edit was successful
+    assert 'egg' in data['labels'] and 'bacon' in data['labels']
