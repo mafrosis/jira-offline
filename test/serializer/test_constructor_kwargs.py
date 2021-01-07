6a51492e93201ca349563e08c5c1b7331408d2d2
Matt Black <github@mafro.net>
2021-01-08 08:17:19 +1100

    [feature] Add tests for constructor_kwargs parameter in serializer refs 88bf239

---
+++ test/serializer/test_constructor_kwargs.py
@@ -0,0 +1,20 @@
+'''
+Tests for the DataclassSerializer.deserialize special-case parameter constructor_kwargs
+'''
+from dataclasses import dataclass, field
+
+from jira_offline.utils.serializer import DataclassSerializer
+
+
+@dataclass
+class Test(DataclassSerializer):
+    m: int
+    r: int = field(repr=False, metadata={'rw': ''})
+
+
+def test_deserialize_extra_kwargs_passed_to_target_class():
+    """
+    Test deserialize passes constructor_kwargs onto the target dataclass after deserialisation
+    """
+    obj = Test.deserialize({'m': 123}, constructor_kwargs={'r': 456})
+    assert obj.r == 456
