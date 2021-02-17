a167ff5715abbcfd25dbe426128706e6972618db
Matt Black <github@mafro.net>
2021-02-18 07:11:12 +1000

    [feature] Distinguish between upstream merges and local merges

--- test/sync/test_merge_issues.py
+++ test/sync/test_merge_issues.py
@@ -19,7 +19,7 @@ def test_merge_issues__doesnt_call_conflict_resolution_on_NO_conflict(mock_build
     # mock build_update to return NO conflicts
     mock_build_update.return_value = IssueUpdate(merged_issue=issue1)
 
-    merge_issues(issue1, issue1)
+    merge_issues(issue1, issue1, is_upstream_merge=True)
 
     # ensure build_update is called
     assert mock_build_update.called is True
@@ -35,7 +35,7 @@ def test_merge_issues__merged_issue_has_original_property_updated_to_match_upstr
     local_issue = Issue.deserialize(ISSUE_1_WITH_FIXVERSIONS_DIFF)
     updated_issue = Issue.deserialize(ISSUE_1_WITH_ASSIGNEE_DIFF)
 
-    update_obj = merge_issues(local_issue, updated_issue)
+    update_obj = merge_issues(local_issue, updated_issue, is_upstream_merge=True)
 
     serialized_upstream_issue = updated_issue.serialize()
     del serialized_upstream_issue['diff_to_original']
@@ -52,9 +52,63 @@ def test_merge_issues__merged_issue_has_diff_to_original():
     local_issue = Issue.deserialize(ISSUE_1_WITH_FIXVERSIONS_DIFF)
     updated_issue = Issue.deserialize(ISSUE_1_WITH_ASSIGNEE_DIFF)
 
-    update_obj = merge_issues(local_issue, updated_issue)
+    update_obj = merge_issues(local_issue, updated_issue, is_upstream_merge=True)
 
     assert update_obj.merged_issue.diff_to_original != []
+
+
+@mock.patch('jira_offline.sync.build_update')
+def test_merge_issues__is_upstream_merge_equals_true__merged_issue_original_equals_updated_issue(mock_build_update):
+    '''
+    Ensure the Issue returned from merge_issues has Issue.set_original() called,
+    when is_upstream_merge=True
+    '''
+    local_issue = Issue.deserialize(ISSUE_1)
+    updated_issue = Issue.deserialize(ISSUE_1_WITH_ASSIGNEE_DIFF)
+
+    # mock build_update to return a valid IssueUpdate object
+    update_obj = IssueUpdate(
+        merged_issue=local_issue,
+        modified={'assignee'},
+    )
+    mock_build_update.return_value = update_obj
+
+    # confirm Issue.original matches `local_issue`
+    assert update_obj.merged_issue.original == local_issue.serialize()
+
+    update_obj = merge_issues(local_issue, updated_issue, is_upstream_merge=True)
+
+    serialized_upstream_issue = updated_issue.serialize()
+    del serialized_upstream_issue['diff_to_original']
+    del serialized_upstream_issue['modified']
+
+    # validate Issue.original updated to match `updated_issue`
+    assert update_obj.merged_issue.original == serialized_upstream_issue
+
+
+@mock.patch('jira_offline.sync.build_update')
+def test_merge_issues__is_upstream_merge_equals_false__merged_issue_original_DOES_NOT_equal_updated_issue(mock_build_update):
+    '''
+    Ensure the Issue returned from merge_issues does not have Issue.set_original() called,
+    when is_upstream_merge=False
+    '''
+    local_issue = Issue.deserialize(ISSUE_1)
+    updated_issue = Issue.deserialize(ISSUE_1_WITH_ASSIGNEE_DIFF)
+
+    # mock build_update to return a valid IssueUpdate object
+    update_obj = IssueUpdate(
+        merged_issue=local_issue,
+        modified={'assignee'},
+    )
+    mock_build_update.return_value = update_obj
+
+    # confirm Issue.original matches `local_issue`
+    assert update_obj.merged_issue.original == local_issue.serialize()
+
+    update_obj = merge_issues(local_issue, updated_issue, is_upstream_merge=False)
+
+    # validate Issue.original still matches `local_issue`
+    assert update_obj.merged_issue.original == local_issue.serialize()
 
 
 @mock.patch('jira_offline.sync.manual_conflict_resolution')
@@ -77,7 +131,7 @@ def test_merge_issues__returns_result_of_manual_conflict_resolution(mock_build_u
     # mock manual_conflict_resolution to return updated issue
     mock_manual_conflict_resolution.return_value = updated_issue
 
-    update_obj = merge_issues(local_issue, updated_issue)
+    update_obj = merge_issues(local_issue, updated_issue, is_upstream_merge=True)
 
     # ensure build_update AND manual_conflict_resolution are called
     mock_build_update.assert_called_once_with(local_issue, updated_issue)
