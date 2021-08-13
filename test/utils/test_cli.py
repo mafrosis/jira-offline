ba5ded9f464b6e57a0b9f7aeaacddcaf273aca17
Matt Black <github@mafro.net>
2021-08-14 09:56:25 +1000

    [fix] Bug fix display of abbreviated UUID keys

--- test/utils/test_cli.py
+++ test/utils/test_cli.py
@@ -17,13 +17,13 @@ def test_print_list__display_ls_fields_config_rendered_in_listing(mock_jira):
     # add fixture to Jira dict
     mock_jira['TEST-71'] = Issue.deserialize(ISSUE_1)
 
-    mock_jira.config.display.ls_fields = {'key'}
+    mock_jira.config.display.ls_fields = {'summary'}
 
     with mock.patch('jira_offline.utils.cli.jira', mock_jira), \
         mock.patch('jira_offline.jira.jira', mock_jira):
         df = print_list(mock_jira.df)
 
-    assert set(df.columns) == {'key'}
+    assert set(df.columns) == {'summary'}
 
 
 def test_print_list__display_ls_fields_defaults_rendered_in_listing(mock_jira):
