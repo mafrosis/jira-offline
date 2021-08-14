03efb20932b1721af453d2985f9c2a188a070307
Matt Black <github@mafro.net>
2021-08-14 12:13:51 +1000

    [version] 0.2.5

--- jira_offline/__init__.py
+++ jira_offline/__init__.py
@@ -3,7 +3,7 @@
 #
 
 __title__ = 'jira-offline'
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 __author__ = 'mafrosis'
 __license__ = 'Simplified MIT License'
 __copyright__ = 'Copyright 2019-2021 Matt Black'
