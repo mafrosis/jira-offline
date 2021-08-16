c87566731331fc2774fae93f03824179adb9bacd
Matt Black <github@mafro.net>
2021-08-16 12:15:30 +1000

    [feature] Allow users to change their auth details after a clone

--- jira_offline/cli/project.py
+++ jira_offline/cli/project.py
@@ -4,7 +4,9 @@ Subcommand for project operations
 import click
 from tabulate import tabulate
 
+from jira_offline.auth import authenticate
 from jira_offline.cli.params import global_options
+from jira_offline.exceptions import ProjectNotConfigured
 from jira_offline.jira import jira
 from jira_offline.utils import find_project
 
@@ -33,6 +35,43 @@ def cli_project_list(ctx: click.core.Context):
             headers=['Key', 'Name', 'Project URI', 'Last Sync'],
             tablefmt='psql'
         ))
+
+
+@cli_projects.command(name='update-auth')
+@click.argument('projectkey')
+@click.option('--username', help='Basic auth username to authenicate with')
+@click.option('--password', help='Basic auth password (use with caution!)')
+@click.option('--oauth-app', default='jira-offline', help='Jira Application Link consumer name')
+@click.option('--oauth-private-key', help='oAuth private key', type=click.Path(exists=True))
+@click.option('--ca-cert', help='Custom CA cert for the Jira server', type=click.Path(exists=True))
+@click.pass_context
+@global_options
+def cli_project_update_auth(ctx: click.core.Context, projectkey: str, username: str=None,
+                            password: str=None, oauth_app: str=None, oauth_private_key: str=None,
+                            ca_cert: str=None):
+    '''
+    Update the authentication on a project
+    '''
+    if username and oauth_private_key:
+        click.echo('You cannot supply both username and oauth params together')
+        raise click.Abort
+
+    try:
+        project = find_project(jira, projectkey)
+    except ProjectNotConfigured:
+        click.echo('Unknown project!')
+        ctx.invoke(cli_project_list)
+        raise click.Abort
+
+    authenticate(project, username, password, oauth_app, oauth_private_key)
+    click.echo(f'Authenticated with {project.jira_server}')
+
+    # store CA cert, if supplied for this Jira
+    if ca_cert:
+        project.set_ca_cert(ca_cert)
+
+    jira.config.write_to_disk()
+    click.echo(f'Updated details for project {projectkey}')
 
 
 @cli_projects.command(name='delete')
