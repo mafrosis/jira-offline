b76083682856d846ce4b5e753ff5d1e8d6db5294
Matt Black <github@mafro.net>
2021-09-30 09:58:18 +1000

    [test][integration] Test for epic linking on existing issues

--- test/integration/test_multiple_issue_push.py
+++ test/integration/test_multiple_issue_push.py
@@ -18,3 +18,18 @@ def test_push__create_with_epic_link(jira_project, run_in_docker):
     output = run_in_docker(jira_project, '--verbose push')
 
     assert 'Pushed 3 of 3 issues' in output
+
+
+#@pytest.mark.integration
+#def test_push__create_and_then_link_after(jira_project, run_in_docker):
+#    '''
+#    Ensure story and epic can be linked, and subsequently pushed
+#    '''
+#    output = run_in_docker(jira_project, f'new {jira_project} Story stozza1')
+#    import ipdb; ipdb.set_trace()
+#    run_in_docker(jira_project, f'new {jira_project} Epic EGGpic --epic-name EGGpic')
+#    run_in_docker(jira_project, f'edit {story_uuid} --epic-link EGGpic')
+#
+#    output = run_in_docker(jira_project, '--verbose push')
+#
+#    assert 'INFO: Pushed 3 of 3 issues' in output
